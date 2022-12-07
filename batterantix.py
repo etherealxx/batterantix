b38b4337b89896c97c98b2fed827a6e9607bd752
etherealxx <gwathon3@gmail.com>
2022-12-08 04:09:26 +0700

    tweaking some codes to run smoothly everywhere

--- batterantix.py
+++ batterantix.py
@@ -2,9 +2,12 @@
 import time
 import os
 import getpass
-currentUser = getpass.getuser()
+
 lowBatteryPercentage = 10
 criticalBatteryPercentage = 4
+
+curdir = os.getcwd()
+currentUser = getpass.getuser()
 os.system('zenity --info --text="batterantix sedang berjalan di backround oleh user ' + currentUser + '" --timeout=3')
 def powerFunction():
     current_stat=open("/sys/class/power_supply/BAT0/capacity","r").readline().strip()
@@ -21,12 +24,12 @@ while True:
     if triggerOnce == 0:
         if int(powerStat)<=int(lowBatteryPercentage) and chargeStat == "Discharging":
             if int(powerStat)<=int(criticalBatteryPercentage):
-                os.system('play -q -v 0.3 /opt/win10-lowbatt.mp3 & zenity --warning --text="Sistem akan segera dimatikan" --timeout=5')
+                os.system('play -q -v 0.3 ' + curdir + '/win10-lowbatt.mp3 & zenity --warning --text="Sistem akan segera dimatikan" --timeout=5')
                 #os.system('poweroff')
                 triggerOnce = 1
             else:
-                os.system('play -q -v 0.3 /opt/win10-lowbatt.mp3 & zenity --warning --text="Baterai tersisa ' + powerStat + '%" --timeout=5')
+                os.system('play -q -v 0.3 ' + curdir + '/win10-lowbatt.mp3 & zenity --warning --text="Baterai tersisa ' + powerStat + '%" --timeout=5')
                 triggerOnce = 1
         elif int(powerStat)>=99 and chargeStat == "Full":
-            os.system('play -q -v 0.3 /opt/win10-lowbatt.mp3 & zenity --warning --text="Baterai penuh" --timeout=5')
+            os.system('play -q -v 0.3 ' + curdir + '/win10-lowbatt.mp3 & zenity --warning --text="Baterai penuh" --timeout=5')
             triggerOnce = 1
